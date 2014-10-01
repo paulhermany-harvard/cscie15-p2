3273a7bc12f9a96b558915276f008b1ff30be3a4
Paul Hermany <paulhermany@gmail.com>
2014-09-30 21:32:56 -0400

    Fixed min/max validation issue

--- js/app.js
+++ js/app.js
@@ -25,17 +25,10 @@ requirejs.config({
 
 require(['jquery', 'bootstrapValidator'], function ($) {
 
-    /* when a grade level is selected, we need to:
-        1. recalculate the new absolute min/max based on selected grade levels
-        2. adjust the min/max length
-        3. recreate the validator to accomodate the changed value
-    */
     $('#gradeLevel').change(function(e) {
-        // set the absolute min/max to the opposite values
         var absMax = $('#minLength').attr('min');
         var absMin = $('#maxLength').attr('max');
         
-        // search through the selected grade levels for the absolute min/max
         $('#gradeLevel option:selected').each(function() {
             var relMax = $(this).data('rel-max');
             if(relMax > absMax) absMax = relMax;
@@ -43,28 +36,46 @@ require(['jquery', 'bootstrapValidator'], function ($) {
             var relMin = $(this).data('rel-min');
             if(relMin < absMin) absMin = relMin;
         });
-
-        // make sure the min and max length are within the absolute limits
+        
         if($('#maxLength').val() > absMax) { $('#maxLength').val(absMax); }
         if($('#minLength').val() < absMin) { $('#minLength').val(absMin); }
-
-        // set the min/max attribute on the length inputs
+        
         $('#maxLength').attr('max', absMax);
         $('#minLength').attr('min', absMin);
-
-        // recreate the max length validator
+        
         $('#chbs').bootstrapValidator('removeField', 'maxLength');
         $('#chbs').bootstrapValidator('addField', 'maxLength');
-
-        // recreate the min length validator
+        
         $('#chbs').bootstrapValidator('removeField', 'minLength');
         $('#chbs').bootstrapValidator('addField', 'minLength');
     });
 
-    // instantiate the bootstrap validator with default settings
-    $('#chbs').bootstrapValidator({})
+    $('#chbs').bootstrapValidator({
+        fields: {
+            minLength: {
+                validators: {
+                    between: {
+                        inclusive: true,
+                        min: 'minLength',
+                        max: 'maxLength',
+                        message: 'The minimum length must be less than or equal to the maximum length'
+                    }
+                }
+            },
+            maxLength: {
+                validators: {
+                    between: {
+                        inclusive: true,
+                        min: 'minLength',
+                        max: 'maxLength',
+                        message: 'The maximum length must be greater than or equal to the minimum length'
+                    }
+                }
+            }
+        }
+    })
     .on('success.field.bv', function(e, data) {
-        // remove success class (too much styling)
+        // remove success class
         data.element.parents('.form-group').removeClass('has-success');
     });
 });

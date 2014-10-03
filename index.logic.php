22f02a2f284bf83b5b6f79cb7be0158f5d2c7468
Paul Hermany <paulhermany@gmail.com>
2014-10-03 09:01:41 -0400

    some code comments and formatting

--- index.logic.php
+++ index.logic.php
@@ -3,6 +3,9 @@
     require 'index.config.php';
 
     // PARSE GRADE LEVEL
+    // - valid values for grade level include G1, G2, G3, ..., G8, SAT
+    // - these values are validated via a regular expression
+    // - if the grade level cannot be validated, the default value is used
     $gradeLevel = $gradeLevel_Constraints['default'];
     if(isset($_REQUEST[$gradeLevel_ParamName])) {
         $usr_gradeLevelArr = $_REQUEST[$gradeLevel_ParamName];
@@ -15,14 +18,21 @@
         }
     }
     
-    // find the relative minimum/maximum length based on selected grade level
+    // Find the relative minimum/maximum length based on selected grade level.
+    // Ex: the minimum length of the set of SAT words is 3,
+    //       while the minimum length for G1 words is 1.
     if(count($gradeLevel) == 0) {
         $relMin = $minLength_Constraints['min'];
         $relMax = $maxLength_Constraints['max'];
     } else {
+        // start from the opposite min/max value
+        //   and work towards the relative min/max value
         $relMin = $minLength_Constraints['max'];
         $relMax = $minLength_Constraints['min'];
-            
+        
+        // iterate through the grade level array,
+        //   get the constraints for each level,
+        //   and adjust relative min/max
         foreach($gradeLevel as $key => $value) {
             $min = $gradeLevel_Constraints['values'][$key][1];
             $max = $gradeLevel_Constraints['values'][$key][2];
@@ -33,6 +43,8 @@
     }
     
     // PARSE WORD COUNT
+    // - valid values include integers between 1 and 10 inclusive
+    // - if the word count cannot be validated, the default value is used
     $wordCount = $wordCount_Constraints['default'];
     if(isset($_REQUEST[$wordCount_ParamName])) {
         $usr_wordCount = $_REQUEST[$wordCount_ParamName];
@@ -43,6 +55,8 @@
     }
     
     // PARSE MINIMUM WORD LENGTH
+    // - valid values include integers within the relative min/max based on grade level
+    // - if the minimum length cannot be validated, the default relative min is used
     $minLength = $relMin;
     if(isset($_REQUEST[$minLength_ParamName])) {
         $usr_minLength = $_REQUEST[$minLength_ParamName];
@@ -53,6 +67,10 @@
     }
 
     // PARSE MAXIMUM WORD LENGTH
+    // - valid values include integers greater than or equal to the minimum length
+    //     and within the relative min/max based on grade level
+    // - if the maximum length cannot be validated, the default relative max is used
+    //     (this should be greater than or equal to the minimum length)
     $maxLength = $relMax;
     if(isset($_REQUEST[$maxLength_ParamName])) {
         $usr_maxLength = $_REQUEST[$maxLength_ParamName];
@@ -63,6 +81,9 @@
     }
     
     // PARSE SEPARATOR CHARACTER(S)
+    // - valid values include any string of 10 characters or less
+    //     (validated via regular expression)
+    // - if the separator cannot be validated, the default separator is used
     $separator = $separator_Constraints['default'];
     if(isset($_REQUEST[$separator_ParamName])) {
         $usr_separator = $_REQUEST[$separator_ParamName];
@@ -72,14 +93,16 @@
     }
     
     // PARSE COMPLEXITY
-	
-	// .. parse "add"
+    
+    // .. parse "add"
+    // - a boolean value indicating that complexity characters should be used
     $complexity_add = $complexity_add_Constraints['default'];
     if(isset($_REQUEST[$complexity_add_ParamName])) {
         $complexity_add = true;
     }
     
-	// .. parse "a random"
+    // .. parse "a random"
+    // - "symbol" or "number"
     $complexity_a_random = $complexity_a_random_Constraints['default'];
     if(isset($_REQUEST[$complexity_a_random_ParamName])) {
         $usr_complexity_a_random = $_REQUEST[$complexity_a_random_ParamName];
@@ -91,7 +114,8 @@
         }
     }
 
-	// .. parse "to the"
+    // .. parse "to the"
+    // - "beginning" or "end"
     $complexity_to_the = $complexity_to_the_Constraints['default'];
     if(isset($_REQUEST[$complexity_to_the_ParamName])) {
         $usr_complexity_to_the = $_REQUEST[$complexity_to_the_ParamName];
@@ -103,7 +127,8 @@
         }
     }
 
-	// .. parse "of"
+    // .. parse "of"
+    // - "each word" or "the password"
     $complexity_of = $complexity_of_Constraints['default'];
     if(isset($_REQUEST[$complexity_of_ParamName])) {
         $usr_complexity_of = $_REQUEST[$complexity_of_ParamName];
@@ -116,18 +141,22 @@
     }   
     
     // SQL injection for grade level
-	// the injected values have already been validated via regular expression
+    // the injected values have already been validated via regular expression
+    //   so this type of SQL injection should be safe
     $gradeLevelInjection = '';
     if(count($gradeLevel) > 0) {
         $csv = [];
+        // iterate through the grade levels and add the single-quoted value to the csv array
         foreach($gradeLevel as $key => $value) {
             array_push($csv, "'".$key."'");
         }
+        // implode the csv array and create the injection statement
         $gradeLevelInjection = ' and level in (' . implode(',', $csv) . ')';
     }
         
     // SQL injection for length range
-	// the injected values have already been validated via intval
+    // the injected values have already been validated via intval
+    //   so this type of SQL injection should be safe
     $lengthRangeInjection = ($minLength == $relMin && $maxLength == $relMax) ? "" : 
         " and (length(word) >= " . $minLength . " and length(word) <= " . $maxLength . ")";
         
@@ -137,6 +166,7 @@
     // handle connection errors
     if ($mysqli->connect_errno) {
         // Unable to connect to database
+        // - no error handling here, just fail miserably
     }
     
     // get the maximum id which is used to choose a random id
@@ -144,15 +174,14 @@
     
     $stmt = $mysqli->prepare($sql);
 
-	// bind and execute
+    // bind and execute
+    // no parameters to bind
     $stmt->execute();
     $stmt->bind_result($maxid);
-    if(!$stmt->fetch()) {
-        // unable to query database
-    }
+    $stmt->fetch();
     $stmt->close();
     
-	// store the generated words in an array for later processing
+    // store the generated words in an array for later processing
     $words = [];
     
     // get a "random" word with the desired grade level
@@ -164,66 +193,72 @@
         // choose the random id
         $id = mt_rand(0, $maxid - 1);
 
-		// bind and execute
+        // bind and execute
         $stmt->bind_param("i", $id);
         $stmt->execute();
         $stmt->bind_result($word);
         
         $stmt->fetch();
         
-		// add the word to the array
+        // add the word to the array
         array_push($words, $word);
     }
     $stmt->close();
     $mysqli->close();
 
-	// generate a plain password (no complexity)
+    // generate a plain password (no complexity)
     $password = implode($separator, $words);
     
-	function generateRandomCharacter($type) {
+    // function to generate a random character of the specified type ("symbol" or "number")
+    // calls the following functions:
+    //   generateRandomNumber
+    //   generateRandomSymbol
+    function generateRandomCharacter($type) {
         switch($type) {
             case 'symbol': return generateRandomSymbol(); break;
             case 'number': return generateRandomNumber(); break;
         }
-	}
-	
-	function generateRandomNumber() {
-		return chr(mt_rand(48, 57));
-	}	
-	
-	function generateRandomSymbol() {
-		$r = mt_rand(0, 3);
-		switch($r) {
-			case 0: $i = mt_rand(33, 47); break;
-			case 1: $i = mt_rand(58, 64); break;
-			case 2: $i = mt_rand(91, 96); break;
-			case 3: $i = mt_rand(123, 126); break;
-		}
-		return chr($i);
-	}
-	
-	// add password complexity based on selected options
+    }
+
+    // function to generate a random number based on ASCII code
+    function generateRandomNumber() {
+        return chr(mt_rand(48, 57));
+    }   
+
+    // function to generate a random symbol based on ASCII code
+    function generateRandomSymbol() {
+        $r = mt_rand(0, 3);
+        switch($r) {
+            case 0: $i = mt_rand(33, 47); break;
+            case 1: $i = mt_rand(58, 64); break;
+            case 2: $i = mt_rand(91, 96); break;
+            case 3: $i = mt_rand(123, 126); break;
+        }
+        return chr($i);
+    }
+    
+    // add password complexity based on selected options
     if($complexity_add == true) {
-		// choose whether to prepend or append the character
+        // choose whether to prepend or append the character
         switch($complexity_to_the) {
             case 'beginning': $prepend = true; break;
             case 'end': $prepend = false; break;
         }
-		// choose the location at which to prepend/append the character
+        // choose the location at which to prepend/append the character
         switch($complexity_of) {
             case 'each word':       
                 for($i = 0; $i < count($words); $i++) {
-					$c = generateRandomCharacter($complexity_a_random);
+                    $c = generateRandomCharacter($complexity_a_random);
                     $words[$i] = $prepend ? ($c.$words[$i]) : ($words[$i].$c);
                     $password = implode($separator, $words);
                 }
                 break;
             case 'the password':
-				$c = generateRandomCharacter($complexity_a_random);
+                $c = generateRandomCharacter($complexity_a_random);
                 $password = $prepend ? ($c.$password) : ($password.$c);
                 break;
         }
     }
 
-	// $password contains the generated password	
-	$password = htmlentities($password);
+    // $password contains the generated password    
+    $password = htmlentities($password);
--- js/app.js
+++ js/app.js
@@ -25,10 +25,12 @@ requirejs.config({
 
 require(['jquery', 'bootstrapValidator'], function ($) {
 
+    // when the grade level changes, recalculate the min/max length and reset the validators
     $('#gradeLevel').change(function(e) {
         var absMax = $('#minLength').attr('min');
         var absMin = $('#maxLength').attr('max');
         
+        // iterate through the selected grade level options to determine the new min/max length
         $('#gradeLevel option:selected').each(function() {
             var relMax = $(this).data('rel-max');
             if(relMax > absMax) absMax = relMax;
@@ -36,30 +38,35 @@ require(['jquery', 'bootstrapValidator'], function ($) {
             var relMin = $(this).data('rel-min');
             if(relMin < absMin) absMin = relMin;
         });
-        
+
+        // set the min/max length to the new absolute min/max
         if($('#maxLength').val() > absMax) { $('#maxLength').val(absMax); }
         if($('#minLength').val() < absMin) { $('#minLength').val(absMin); }
-        
+
+        // set the html5 mim/max attributes for validation
         $('#maxLength').attr('max', absMax);
         $('#minLength').attr('min', absMin);
-        
+
+        // reset the max length validator
         $('#chbs').bootstrapValidator('removeField', 'maxLength');
         $('#chbs').bootstrapValidator('addField', 'maxLength');
-        
+
+        // reset the min length validator
         $('#chbs').bootstrapValidator('removeField', 'minLength');
-        $('#chbs').bootstrapValidator('addField', 'minLength');
-		
-		
+        $('#chbs').bootstrapValidator('addField', 'minLength');     
     });
 
-	$('#minLength').change(function(e) {
-		$('#chbs').bootstrapValidator('revalidateField', 'maxLength');
-	});
+    // when the minimum length changes, revalidate the max length field
+    $('#minLength').change(function(e) {
+        $('#chbs').bootstrapValidator('revalidateField', 'maxLength');
+    });
 
-	$('#maxLength').change(function(e) {
-		$('#chbs').bootstrapValidator('revalidateField', 'minLength');
-	});
+    // when the maximum length changes, revalidate the min length field
+    $('#maxLength').change(function(e) {
+        $('#chbs').bootstrapValidator('revalidateField', 'minLength');
+    });
 
+    // setup the bootstrap validator
     $('#chbs').bootstrapValidator({
     })
     .on('success.field.bv', function(e, data) {
